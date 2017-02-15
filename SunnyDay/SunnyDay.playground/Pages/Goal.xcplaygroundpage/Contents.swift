fccbcc4b1546a2e75e7f749252573993ea384ed9
Student <student@macbook-pro-869-lefeuvre.local>
2017-02-15 10:41:46 -0500

    I finished the sun and the clouds. I am now currently working on the rainbow.

--- SunnyDay/SunnyDay.playground/Pages/Goal.xcplaygroundpage/Contents.swift
+++ SunnyDay/SunnyDay.playground/Pages/Goal.xcplaygroundpage/Contents.swift
@@ -16,11 +16,83 @@ import PlaygroundSupport
 // Create a new canvas
 let canvas = Canvas(width: 500, height: 400)
 
+//Draw Blue Rectangle
+canvas.fillColor = Color(hue: 195, saturation: 100, brightness: 100, alpha: 100)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 400, borderWidth: 0)
+2
 // Draw a circle in the middle of the canvas without a fill
 canvas.drawShapesWithBorders = true
-canvas.drawShapesWithFill = false
+canvas.drawShapesWithFill = true
 canvas.defaultBorderWidth = 5
 canvas.drawEllipse(centreX: canvas.width / 2, centreY: canvas.height / 2 , width: 100, height: 100)
+
+//Examples of using the remainder operator
+// and integer division
+9 / 3
+9 % 3
+17 / 3
+17 % 3
+
+// Draw lines
+canvas.defaultLineWidth = 5
+
+// Use a loop to do the sun rays
+
+for x in stride(from: 0, through: 500, by: 50) {
+    
+    
+    
+    // Choose the color
+    if x % 100 == 0 {
+        canvas.lineColor = Color.yellow
+    } else {
+        canvas.lineColor = Color.orange
+    }
+    
+    //Draw the sun ray
+    canvas.drawLine(fromX:0, fromY:400,toX:x, toY: 0)
+    
+    
+}
+
+for y in stride(from: 0, through: 400, by: 50) {
+    
+    // Choose the color
+    if y % 100 == 0 {
+        canvas.lineColor = Color.yellow
+    } else {
+        canvas.lineColor = Color.orange
+    }
+    
+    //Draw the sun ray
+    canvas.drawLine(fromX:0, fromY:400, toX:500, toY: y)
+    
+    
+    
+}
+
+//Draw yellow circle
+canvas.fillColor = Color.yellow
+canvas.drawShapesWithBorders = false
+canvas.drawEllipse(centreX: 0, centreY: 400, width: 80, height: 80)
+
+//Draw white circle (Cloud)
+canvas.fillColor = Color.white
+canvas.drawShapesWithBorders = false
+canvas.drawEllipse(centreX: 480, centreY: 400, width: 100, height: 100)
+
+//Draw 2nd white circle (Cloud)
+canvas.fillColor = Color.white
+canvas.drawShapesWithBorders = false
+canvas.drawEllipse(centreX: 510, centreY: 360, width: 100, height: 100)
+
+//First line of Rainbow
+canvas.borderColor = Color.red
+canvas.drawShapesWithBorders = true
+canvas.drawShapesWithFill = false
+canvas.defaultBorderWidth = 30
+canvas.drawEllipse(centreX: 250, centreY: 20, width: 200, height: 80)
+
 
 //: ## Template code
 //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
